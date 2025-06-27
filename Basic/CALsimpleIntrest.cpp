f4562ceb6fd4f78a4575bd36cf0313d98590b09d
Pranshu Gupta <157245518+PranshuGupta28@users.noreply.github.com>
2025-06-27 12:46:26 +0530

    CALsimpleIntrest.cpp

---
+++ Basic/CALsimpleIntrest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main () {
+    float Princple = 2500 , Rate = 10 , Time = 2,
+    Intrest ;
+
+
+       Intrest = (Princple*Rate*Time)/100;
+
+       cout<<Intrest;     
+
+} 
